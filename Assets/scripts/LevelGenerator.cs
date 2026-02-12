115b5d62497cb2b96f9571e8ad464e9f601b0f5e
Names <40932@ma-web.nl>
2026-02-12 14:52:38 +0100

    grid added

---
+++ Assets/scripts/LevelGenerator.cs
@@ -0,0 +1,102 @@
+using System.Linq;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject wallPrefab;
+    public GameObject dotPrefab;
+    public GameObject playerPrefab;
+    int s;
+
+    string[][] levelData = {new string[]{
+        "###############",
+        "#.............#",
+        "#.##.##.......#",
+        "#.............#",
+        "#.............#",
+        "#.............#",
+        "#.............#",
+        "#....P........#",
+        "#.............#",
+        "###############",
+    },
+      new string[]  {
+        "###############",
+        "#.............#",
+        "#.##.##.......#",
+        "#.............#",
+        "#....P........#",
+        "#.............#",
+        "#.............#",
+        "#.#########...#",
+        "#.............#",
+        "###############",
+
+
+    },
+      new string[]
+      {
+        "###############",
+        "#.............#",
+        "#.##.##.......#",
+        "#.............#",
+        "#...P....###..#",
+        "#..###........#",
+        "#.............#",
+        "#.#########...#",
+        "#.............#",
+        "###############"
+      }
+    };
+
+    
+
+    void Start()
+    {
+       
+        GenerateLevel();
+    }
+
+
+    void GenerateLevel()
+    {
+       s = Random.Range(0, levelData.Length);
+      
+
+        for (int y = 0; y < levelData[s].Length; y++)
+        {
+            string line = levelData[s][y];
+            if (line == " ")
+            {
+                break;
+
+            }
+            string row = line;
+            for (int x = 0; x < row.Length; x++)
+            {
+                char tile = row[x];
+                Vector3 position = new Vector3(x, -y, 0);
+
+                switch (tile)
+                {
+
+                    case '#':
+                        Instantiate(wallPrefab, position, Quaternion.identity);
+                        break;
+                    case '.':
+                        Instantiate(dotPrefab, position, Quaternion.identity);
+                        break;
+                    case 'P':
+                        Instantiate(playerPrefab, position, Quaternion.identity);
+                        break;
+                    
+       
+
+                }
+            }
+
+
+        }
+       
+    }
+}
