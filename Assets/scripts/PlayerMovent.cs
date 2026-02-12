22d88c4b346b03abce7087e97e4b11a6c3d5d15e
Names <40932@ma-web.nl>
2026-02-12 15:37:55 +0100

    physics material

---
+++ Assets/scripts/PlayerMovent.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerMovent : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+   
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
